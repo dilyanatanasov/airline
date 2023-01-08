6222eb992e6153d1b1c7d5bba3a70b0a7e5cc2ff
Dilyan Atanasov <dilyan.atanasov@covantis.io>
2023-01-08 22:05:36 +0200

    test nest and angular

--- airline-frontend/src/app/app.component.ts
+++ airline-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'airline-frontend';
+  flight: any;
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.httpClient.get("http://localhost:3000/landing").subscribe((res: any) => {
+      this.flight = res.name;
+    });
+  }
 }
