b53a400265209eb33e397809fab2c9dfcfc69241
Dilyan Atanasov <dilyan.atanasov@covantis.io>
2023-01-10 21:44:10 +0200

    progress

--- airline-frontend/src/app/app.component.ts
+++ airline-frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import content from "./translation.json";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,13 +9,42 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  flight: any;
+  pageContent = content["en"];
+  airports = [
+    {
+      id: 1,
+      name: "Varna"
+    },
+    {
+      id: 2,
+      name: "Vienna"
+    }
+  ];
+  fromAirport = this.airports;
+  toAirport = this.airports;
+  flightSearch: FormGroup = new FormGroup({
+    fromAirport: new FormControl("Test"),
+    toAirport: new FormControl(),
+    fromDate: new FormControl(null),
+    toDate: new FormControl(null),
+    numberOfPassengers: new FormControl(null)
+  });
+
   constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit() {
-    this.httpClient.get("http://localhost:3000/landing").subscribe((res: any) => {
-      this.flight = res.name;
-    });
+    this.flightSearch.get("fromAirport")?.setValue(1)
+    this.flightSearch.get("toAirport")?.setValue(2)
+  }
+
+  public getFlights() {
+    const search = {
+      fromAirport: this.flightSearch.get("fromAirport")?.value,
+      toAirport: this.flightSearch.get("toAirport")?.value,
+      fromDate: this.flightSearch.get("fromDate")?.value,
+      toDate: this.flightSearch.get("toDate")?.value,
+    }
+    console.log({ search });
   }
 }
